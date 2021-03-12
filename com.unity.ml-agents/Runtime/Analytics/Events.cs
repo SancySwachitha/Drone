696f4d4bf38d230f4d0e9f32867dad6f4c9e1e33
Christopher Goy <christopherg@unity3d.com>
2021-03-12 15:04:33 -0800

    Make analytics an optional dep.

--- com.unity.ml-agents/Runtime/Analytics/Events.cs
+++ com.unity.ml-agents/Runtime/Analytics/Events.cs
@@ -1,3 +1,4 @@
+#if MLA_UNITY_ANALYTICS
 using System;
 using System.Collections.Generic;
 using Unity.MLAgents.Actuators;
@@ -188,3 +189,4 @@ namespace Unity.MLAgents.Analytics
         public int NumNetworkHiddenUnits;
     }
 }
+#endif // MLA_UNITY_ANALYTICS
